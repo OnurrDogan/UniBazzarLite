7e6f5bcc4d4b3587f2b440b5f23182633253ca46
Anıl <150185838+anils06@users.noreply.github.com>
2025-07-01 21:12:00 +0300

    Update CurrentUserMiddleware.cs
    feyk kullanıcı yetkilendirme middleware'i eklendi

--- UniBazzarLite/Middlewares/CurrentUserMiddleware.cs
+++ UniBazzarLite/Middlewares/CurrentUserMiddleware.cs
@@ -1,24 +1,23 @@
-﻿using System.Security.Claims;
-
-namespace UniBazaarLite.Middlewares;
+using System.Security.Claims;
 
 public class CurrentUserMiddleware
 {
     private readonly RequestDelegate _next;
-    public CurrentUserMiddleware(RequestDelegate next) => _next = next;
+
+    public CurrentUserMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        if (!context.User.Identity?.IsAuthenticated ?? true)
+        var claims = new List<Claim>
         {
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, "11111111-1111-1111-1111-111111111111"),
-                new Claim(ClaimTypes.Name, "demo@unibazaar.local"),
-                new Claim(ClaimTypes.Role, "Student")
-            };
-            context.User = new ClaimsPrincipal(new ClaimsIdentity(claims, "Fake"));
-        }
+            new Claim(ClaimTypes.Name, "testuser@university.edu"),
+            new Claim(ClaimTypes.Role, "Student")
+        };
+        var identity = new ClaimsIdentity(claims, "FakeAuth");
+        context.User = new ClaimsPrincipal(identity);
 
         await _next(context);
     }
